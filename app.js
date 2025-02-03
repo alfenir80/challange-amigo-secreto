5d2c53325a9a71a43759d890bfc55668daa25ac4
alfenir80 <fabriciojanuario@proton.me>
2025-02-03 17:35:55 -0300

    logica final jogo

--- app.js
+++ app.js
@@ -1,31 +1,39 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
 let amigos = [];
+let campo = document.getElementById("listaAmigos");
+function adicionarAmigo(){
 
-function adicionaAmigos(){
-
-    if (document.getElementById("amigo") == "")
+    
+    if (document.getElementById("amigo").value == " ")
         alert ("Campo vazio!");
-    else if (amigos.includes(document.getElementById("amigo")))
+
+
+    if (amigos.includes(document.querySelector("input").value)){
         alert ("Amigo já incluso na lista!");
+    }
     else {
-        amigos.push(document.getElementById("amigo"));
-        document.getElementById("amigo").value = " ";
-        let campo = document.getElementById("listaAmigos");
         
-        for (let i = 0; i <= amigos.length; i++){
-            campo.innerHTML = `<li>${amigos[i]}</li>`;
+        amigos.push(document.getElementById("amigo").value);
+        document.getElementById("amigo").value = " ";
+       
+        console.log(amigos);
+        campo.innerHTML = " ";
+        for (let i = 0; i < amigos.length; i++){
+             
+            campo.innerHTML += `<li>${amigos[i]}</li>`;
         }
-
+        
 }
 
 }
 
-function sortearAmigo(lista){
+function sortearAmigo(){
 
-    let num = parseInt(Math.random() * lista.length + 1);
-    let campo = getElementById("resultado");
-    campo.innerHTML = `<li>${lista[num]}</li>`;
+    let num = parseInt(Math.random() * amigos.length + 1);
+    console.log(num);
+    let campo = document.getElementById("resultado");
+    campo.innerHTML = `<li>${amigos[num - 1]}</li>`;
     
 
 }
