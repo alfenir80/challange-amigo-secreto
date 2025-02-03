830812f857c1a1bd0529351d74c8aec5e0f55669
alfenir80 <fabriciojanuario@proton.me>
2025-02-03 16:10:57 -0300

    logica jogo inicial

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionaAmigos(){
+
+    if (document.getElementById("amigo") == "")
+        alert ("Campo vazio!");
+    else if (amigos.includes(document.getElementById("amigo")))
+        alert ("Amigo já incluso na lista!");
+    else {
+        amigos.push(document.getElementById("amigo"));
+        document.getElementById("amigo").value = " ";
+        let campo = document.getElementById("listaAmigos");
+        
+        for (let i = 0; i <= amigos.length; i++){
+            campo.innerHTML = `<li>${amigos[i]}</li>`;
+        }
+
+}
+
+}
+
+function sortearAmigo(lista){
+
+    let num = parseInt(Math.random() * lista.length + 1);
+    let campo = getElementById("resultado");
+    campo.innerHTML = `<li>${lista[num]}</li>`;
+    
+
+}
